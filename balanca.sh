00a4b3c126ec4962f27e4224466910ad68fec3f2
Jhean Phabio <127798956+jhonnunes443@users.noreply.github.com>
2025-08-21 09:24:26 -0300

    Update balanca.sh

--- balanca.sh
+++ balanca.sh
@@ -2,6 +2,6 @@
 sudo apt install git -y
 sudo apt install python3-pip -y
 
-git clone https://github.com/jhonnunes443/balanca_projeto.git
+git clone https://github.com/jhonnunes443/Balanca_projeto.git
 pip install -r /Balanca_projeto/requirements.txt
 python3 /Balanca_projeto/Balanca_final.py

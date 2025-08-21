c4046b91b584eb7a55f62d505d071e0db1710826
Jhean Phabio <127798956+jhonnunes443@users.noreply.github.com>
2025-08-21 09:26:35 -0300

    Update balanca.sh

--- balanca.sh
+++ balanca.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-sudo apt install git -y
-sudo apt install python3-pip -y
 
-git clone https://github.com/jhonnunes443/Balanca_projeto.git
-pip install -r /Balanca_projeto/requirements.txt
-python3 /Balanca_projeto/Balanca_final.py
+if ! command -v git &> /dev/null; then
+    echo "Git não encontrado. Instalando..."
+    sudo apt install git -y
+else
+    echo "Git já está instalado."
+fi
+
+if ! command -v python3 &> /dev/null || ! command -v pip3 &> /dev/null; then
+    echo "Python3 ou pip3 não encontrados. Instalando..."
+    sudo apt install python3-pip -y
+else
+    echo "Python3 e pip3 já estão instalados."
+fi
+
+if [ ! -d "Balanca_projeto" ]; then
+    git clone https://github.com/jhonnunes443/Balanca_projeto.git
+else
+    echo "Repositório 'Balanca_projeto' já existe. Pulando o clone."
+fi
+
+chmod +x Balanca_projeto/balanca.sh
+./Balanca_projeto/balanca.sh
